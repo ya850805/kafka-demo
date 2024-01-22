0496ad25b793a631ed23689b5743ad2603bc51be
Jason <ya850805@gmail.com>
2024-01-22 18:47:11 +0800

    feat: Add integrate Springboot producer demo.

---
+++ springboot-kafka/src/main/java/org/example/springbootkafka/controller/ProducerController.java
@@ -0,0 +1,25 @@
+package org.example.springbootkafka.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jason
+ * @description
+ * @create 2024/1/22 18:41
+ **/
+@RestController
+public class ProducerController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @RequestMapping("/send")
+    public String data(String msg) {
+        //通過kafka發送出去
+        kafkaTemplate.send("first", msg);
+        return "ok";
+    }
+}
